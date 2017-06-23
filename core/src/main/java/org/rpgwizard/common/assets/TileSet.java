256131d3f6f088f329fb314c09634496840443bc
swordmaster2k <b00056835@gmail.com>
2017-06-23 17:01:13 +0100

    License header amendments, pom version updates.

--- core/src/main/java/org/rpgwizard/common/assets/TileSet.java
+++ core/src/main/java/org/rpgwizard/common/assets/TileSet.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, rpgtoolkit.net <help@rpgtoolkit.net>
+ * Copyright (c) 2015, rpgwizard.org, some files forked from rpgtoolkit.net <info@rpgwizard.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this

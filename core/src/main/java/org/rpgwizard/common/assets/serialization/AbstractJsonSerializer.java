0e733a5631f7d72448319549e33b70f9ad3de651
Joshua Michael Daly <joshua.michael.daly@gmail.com>
2020-04-25 10:05:35 +0100

    Upgrade demo projects to latest asset version

--- core/src/main/java/org/rpgwizard/common/assets/serialization/AbstractJsonSerializer.java
+++ core/src/main/java/org/rpgwizard/common/assets/serialization/AbstractJsonSerializer.java
@@ -43,7 +43,7 @@ import org.rpgwizard.common.assets.board.BoardVectorType;
 public abstract class AbstractJsonSerializer extends AbstractAssetSerializer {
 
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-    public static final double FILE_FORMAT_VERSION = 1.6;
+    public static final double FILE_FORMAT_VERSION = 1.7;
 
     @Override
     public void serialize(AssetHandle handle) throws IOException, AssetException {

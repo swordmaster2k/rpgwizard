7f5125b0f87ace5816b99c0751213ea369c280e7
Joshua Michael Daly <joshua.michael.daly@gmail.com>
2020-10-18 17:10:23 +0100

    Basic ESLint & NPM setup

---
+++ engine/engine-ts/src/script-vm.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017, rpgwizard.org, some files forked from rpgtoolkit.net <info@rpgwizard.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+export class ScriptVM {
+
+    private cache: object;
+
+    constructor() {
+        this.cache = {};
+    }
+
+    public async run(file: string, origin: object) {
+        console.info("running game script file=[%s], origin=[%s]", file, origin);
+
+        // Try to get the game script from the cache
+        let script: any;
+        if (this.cache[file]) {
+            script = this.cache[file];
+        } else {
+            script = await import(file);
+            this.cache[file] = script;
+        }
+
+        // Run the game script, provide the origin
+        await script.default(origin);
+
+        console.info("finished game script file=[%s], origin=[%s]", file, origin);
+    }
+
+}

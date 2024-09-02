58d536c4268809eaf5ff8ffafe478f47b1ba4226
swordmaster2k <joshua.michael.daly@gmail.com>
2024-09-02 19:46:19 +0100

    refactor: start removing old engine plugin

--- editor/editor-ui/src/main/java/org/rpgwizard/editor/ui/actions/AbstractRunAction.java
+++ editor/editor-ui/src/main/java/org/rpgwizard/editor/ui/actions/AbstractRunAction.java
@@ -12,16 +12,13 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
-import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.ProgressMonitor;
 import javax.swing.SwingWorker;
 import org.apache.commons.io.FileUtils;
-import org.pf4j.PluginManager;
 import org.rpgwizard.common.assets.game.Game;
 import org.rpgwizard.editor.MainWindow;
 import org.rpgwizard.editor.utilities.FileTools;
-import org.rpgwizard.pluginsystem.Engine;
 
 /**
  *
@@ -30,7 +27,6 @@ import org.rpgwizard.pluginsystem.Engine;
 public abstract class AbstractRunAction extends AbstractAction {
 
     private ProgressMonitor progressMonitor;
-    private SwingWorker worker;
 
     protected void toggleButtons() {
         MainWindow instance = MainWindow.getInstance();
@@ -40,8 +36,6 @@ public abstract class AbstractRunAction extends AbstractAction {
     }
 
     protected File copyProject() throws IOException, URISyntaxException {
-        // Make a temporary copy of the user's project for the engine to
-        // use.
         File projectOriginal = new File(System.getProperty("project.path"));
         File projectCopy = new File(FileTools.getTempDirectory(),
                 projectOriginal.getName() + "-" + System.currentTimeMillis());
@@ -56,43 +50,36 @@ public abstract class AbstractRunAction extends AbstractAction {
         int projectHeight = project.getViewport().getHeight();
         boolean isFullScreen = project.getViewport().isFullScreen();
 
-        PluginManager pluginManager = instance.getPluginManager();
-        List<Engine> engines = pluginManager.getExtensions(Engine.class);
+        progressMonitor = new ProgressMonitor(instance, "Starting Engine...", "", 0, 100);
+        progressMonitor.setProgress(0);
+        Dimension dimensions = new Dimension(projectWidth, projectHeight);
+        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                runEngine(project, dimensions, isFullScreen, projectCopy, progressMonitor);
+                return null;
+            }
 
-        // Just use the first available engine for now.
-        if (engines.size() > 0) {
-            progressMonitor = new ProgressMonitor(instance, "Starting Engine...", "", 0, 100);
-            progressMonitor.setProgress(0);
-            Dimension dimensions = new Dimension(projectWidth, projectHeight);
-            worker = new SwingWorker<Void, Void>() {
-                @Override
-                protected Void doInBackground() throws Exception {
-                    runEngine(engines.get(0), project, dimensions, isFullScreen, projectCopy, progressMonitor);
-                    return null;
-                }
-
-                @Override
-                public void done() {
-                    instance.getMainToolBar().getStopButton().setEnabled(true);
-                }
-            };
-            worker.execute();
-        }
+            @Override
+            public void done() {
+                instance.getMainToolBar().getStopButton().setEnabled(true);
+            }
+        };
+        worker.execute();
     }
 
-    private void runEngine(Engine engine, Game project, Dimension dimensions, boolean isFullScreen, File projectCopy,
-            ProgressMonitor progressMonitor) throws InterruptedException, InvocationTargetException, Exception {
-        // Get the project icon if available.
+    private void runEngine(
+            Game project,
+            Dimension dimensions,
+            boolean isFullScreen, File projectCopy,
+            ProgressMonitor progressMonitor
+    ) throws InterruptedException, InvocationTargetException, Exception {
         File projectIcon = null; // REFACTOR: Move to game.ico way
-        // engine.run(project.getName(), dimensions.width, dimensions.height, isFullScreen, projectCopy,
-        // progressMonitor,
-        // projectIcon);
 
         String command = "love.exe " + '"' + projectCopy.getAbsolutePath() + File.separator + "game" + '"';
         System.out.println(command);
         Runtime runtime = Runtime.getRuntime();
         Process process = runtime.exec(command);
-
     }
 
     public static void main(String[] args) throws Exception {
